6cf346c7fd29ace2c101c3bcc554a9020ce11013
bioinsilico <jsegurmor@gmail.com>
2024-11-14 08:43:24 -0800

    bug fixed

--- src/db.py
+++ src/db.py
@@ -76,10 +76,10 @@ def load_path_to_collection(embedding_path, *db_collection_list):
                 points=[
                     PointStruct(
                         id=idx,
-                        vector=vector.tolist(),
-                        payload={"color": "red", "rand_number": idx % 10}
+                        vector=vector,
+                        payload={"rcsb_id": rcsb_id}
                     )
-                    for idx, vector in zip(buffer["ids"], buffer["embeddings"])
+                    for idx, (rcsb_id, vector) in enumerate(zip(buffer["ids"], buffer["embeddings"]))
                 ]
             )
     print(f"DB load path {embedding_path} done")

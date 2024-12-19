33fdede7aa5e7f6acc0e0a202085e4d728e1a377
Joan Segura <jsegurmor@gmail.com>
2024-12-18 23:27:06 -0800

    shape to bin with struct

--- src/embedding_diskann_loader.py
+++ src/embedding_diskann_loader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import struct
 
 
 class EmbeddingLoader:
@@ -20,7 +21,8 @@ class EmbeddingLoader:
     def open_bin(self, n_rows, dim):
         with open(self.diskann_bin_file, 'wb') as f:
             f.write(n_rows.to_bytes(4))
-            f.write(dim.to_bytes(4))
+            f.write(struct.pack('<i', n_rows))
+            f.write(struct.pack('<i', dim))
 
     def add_to_bin(self, df):
         if not {self.ID_FIELD, self.EMBEDDING_FIELD}.issubset(df.columns):

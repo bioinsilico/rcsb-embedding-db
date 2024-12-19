71b4390d1d4061a1385ee2bcad8371e6a23eaacd
Joan Segura <jsegurmor@gmail.com>
2024-12-18 20:41:07 -0800

    bug

--- src/load_diskann_embeddings.py
+++ src/load_diskann_embeddings.py
@@ -15,14 +15,15 @@ def main():
     total_len = 0
     df_files = os.listdir(af_embedding_folder)[0:10]
     with tqdm(total=len(df_files), desc="Loading embeddings", unit="file") as pbar:
-        for df in os.listdir(af_embedding_folder):
+        for df in df_files:
             total_len += len(pd.read_pickle(f'{af_embedding_folder}/{df}'))
             pbar.update(1)
+    pbar.close()
     print(f"Saving {total_len} embeddings")
 
     embedding_loader = EmbeddingLoader(diskann_tmp_folder, dim, total_len)
     with tqdm(total=len(df_files), desc="Loading embeddings", unit="file") as pbar:
-        for df in os.listdir(af_embedding_folder):
+        for df in df_files:
             embedding_loader.add_to_bin(f'{af_embedding_folder}/{df}')
             pbar.update(1)
 

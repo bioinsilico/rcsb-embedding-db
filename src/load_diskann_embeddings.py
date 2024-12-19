a354a5a1da3bc7b2022c34f4b85185671e1c3999
Joan Segura <jsegurmor@gmail.com>
2024-12-19 00:05:16 -0800

    numpy shape bin with struct

--- src/load_diskann_embeddings.py
+++ src/load_diskann_embeddings.py
@@ -15,7 +15,7 @@ BATCH_SIZE = 20000
 
 def main():
     total_len = 0
-    df_files = os.listdir(af_embedding_folder)[0:5]
+    df_files = os.listdir(af_embedding_folder)
     with tqdm(total=len(df_files), desc="Loading embeddings", unit="file") as pbar:
         for _df in df_files:
             df = pd.read_pickle(f'{af_embedding_folder}/{_df}')

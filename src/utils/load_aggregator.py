32d2da39b6f9de409eada9cc43b88483a67a41c5
bioinsilico <jsegurmor@gmail.com>
2025-02-25 15:56:52 -0800

    indexing only arg name

--- src/utils/load_aggregator.py
+++ src/utils/load_aggregator.py
@@ -1,19 +1,6 @@
-
-from lightning_module.inference.embedding_inference import LitEmbeddingInference
-from networks.transformer_nn import TransformerEmbeddingCosine
+import torch
 
 
 def load_aggregator(checkpoint):
-    nn_model = TransformerEmbeddingCosine(
-        input_features=1536,
-        nhead=12,
-        num_layers=6,
-        dim_feedforward=3072,
-        hidden_layer=1536,
-        res_block_layers=12
-    )
-
-    return LitEmbeddingInference.load_from_checkpoint(
-        checkpoint,
-        nn_model=nn_model
-    ).model
+    model = torch.load(checkpoint, weights_only=True)
+    return model

b63983de87aac70ca0689d1ae01d42c4f9e49243
bioinsilico <jsegurmor@gmail.com>
2025-03-11 14:44:06 -0700

    shuffle af files

--- src/scripts/load_af_embeddings.py
+++ src/scripts/load_af_embeddings.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from random import shuffle
 
 import pandas as pd
 from tqdm import tqdm
@@ -28,7 +29,9 @@ def main(af_embedding_folder, index_only):
     embedding_loader.create_embedding_collection()
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [executor.submit(__insert_file, f'{af_embedding_folder}/{df}') for df in os.listdir(af_embedding_folder)]
+        af_files = [df for df in os.listdir(af_embedding_folder)]
+        shuffle(af_files)
+        futures = [executor.submit(__insert_file, f'{af_embedding_folder}/{df}') for df in af_files]
         with tqdm(total=len(futures), desc="Loading embeddings", unit="file") as pbar:
             for _ in concurrent.futures.as_completed(futures):
                 pbar.update(1)
